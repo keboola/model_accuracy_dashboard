fixing MAPE calculation

pages/02_Individual_Forecasts.py
@@ -3,9 +3,10 @@ from src.settings import ACCURACY_MONITORING_MEALS_TAB, ACCURACY_MONITORING_TAB,
 from src.graphs import preprocess_data, create_series_plot_new
 from src.graphs import filter_by_date
 from src.helpers import read_df, calculate_categories
+from src.helpers import MAPE
 import pandas as pd
-import numpy as np
 from streamlit_extras.switch_page_button import switch_page
+import datetime
 
 if 'authentication_status' not in st.session_state:
     switch_page("login")
@@ -23,9 +24,8 @@ if st.session_state["authentication_status"]:
         outlet_number = st.selectbox("Outlet Number", cat3)
         category_selected = '~'.join([category_type, outlet_type, outlet_number])
     
-    #meals_preprocessed = preprocess_data(df_meals, category_selected)
     accu_preprocessed = preprocess_data(df_accuracy, category_selected, time_col='ds')
-    #actuals_preprocessed = preprocess_data(df_actuals, category_selected, time_col='ds', freq='15min')
+        
     with st.sidebar:
         sc1, sc2 = st.columns(2)
         with sc1:
@@ -41,34 +41,43 @@ if st.session_state["authentication_status"]:
                                         max_value=accu_preprocessed.dt.max().date())
 
     start_date = pd.to_datetime(start_date)
+    # we need to include the last date of selection
+
+    end_date = end_date + datetime.timedelta(days=1)
     end_date = pd.to_datetime(end_date)     
     #meals_preprocessed_filtered = filter_by_date(meals_preprocessed, start_date, end_date)
     accu_preprocessed_filtered = filter_by_date(accu_preprocessed, start_date, end_date)
-    #actuals_preprocesses_filtered = filter_by_date(actuals_preprocessed, start_date, end_date)   
+
+    # 1. aggregate by category, date and meal_category
+    group_cols = ["category", "date", "meal_category"]
+    agg_cols = ["metric_actual", "metric_forecast", "metric_forecast_lgbm", "metric_forecast_rf"]
+    accu_preprocessed_filtered_agg = accu_preprocessed_filtered.groupby(group_cols)[agg_cols].sum().reset_index()
     
-    sales_lunch_actual = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='lunch'].metric_actual.sum()
-    sales_lunch_forecast_prophet = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='lunch'].metric_forecast.sum()
-    sales_lunch_forecast_lgbm = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='lunch'].metric_forecast_lgbm.sum()
-    sales_lunch_forecast_rf = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='lunch'].metric_forecast_rf.sum()
+    #st.write(accu_preprocessed_filtered_agg)
+    # 2. calculate MAPE
+    mape_lunch_prophet = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_forecast)
 
-    lunch_pcnt_err_prophet = 100* np.abs(sales_lunch_forecast_prophet - sales_lunch_actual)/sales_lunch_actual
-    lunch_pcnt_err_lgbm = 100* np.abs(sales_lunch_forecast_lgbm - sales_lunch_actual)/sales_lunch_actual
-    lunch_pcnt_err_rf = 100* np.abs(sales_lunch_forecast_rf - sales_lunch_actual)/sales_lunch_actual
+    mape_lunch_lgbm = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_forecast_lgbm)
 
-    sales_dinner_actual = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='dinner'].metric_actual.sum()
-    sales_dinner_forecast_prophet = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='dinner'].metric_forecast.sum()
-    sales_dinner_forecast_lgbm = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='dinner'].metric_forecast_lgbm.sum()
-    sales_dinner_forecast_rf = accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category=='dinner'].metric_forecast_rf.sum()
+    mape_lunch_rf = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='lunch'].metric_forecast_rf)
 
-    dinner_pcnt_err_prophet = 100* np.abs(sales_dinner_forecast_prophet - sales_dinner_actual)/sales_dinner_actual
-    dinner_pcnt_err_lgbm = 100* np.abs(sales_dinner_forecast_lgbm - sales_dinner_actual)/sales_dinner_actual
-    dinner_pcnt_err_rf = 100* np.abs(sales_dinner_forecast_rf - sales_dinner_actual)/sales_dinner_actual
+    mape_dinner_prophet = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_forecast)
 
+    mape_dinner_lgbm = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_forecast_lgbm)
+
+    mape_dinner_rf = MAPE(accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_actual, 
+                              accu_preprocessed_filtered_agg.loc[accu_preprocessed_filtered_agg.meal_category=='dinner'].metric_forecast_rf)
 
 
     #if meals_preprocessed_filtered.empty:
     #    st.warning("No data for available for the selection.")
     #    st.stop()
+    
     if accu_preprocessed_filtered.empty:
         st.warning("No data for available for the selection.")
         st.stop()
@@ -76,37 +85,35 @@ if st.session_state["authentication_status"]:
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown("### Average error over lunch periods:")
-            
+            st.markdown("### MAPE (lunch):")
+            st.markdown("(Mean Average Percentage Error)")
+
             col11, col12, col13 = st.columns(3)
             with col11:
-                st.metric("Prophet", f"{lunch_pcnt_err_prophet:.2f} %", f"{15 - lunch_pcnt_err_prophet:.2f} %")
+                st.metric("Prophet", f"{mape_lunch_prophet:.2f} %", f"{15 - mape_lunch_prophet:.2f} %")
             with col12:
-                st.metric("LightGBM", f"{lunch_pcnt_err_lgbm:.2f} %", f"{15 - lunch_pcnt_err_lgbm:.2f} %")
+                st.metric("LightGBM", f"{mape_lunch_lgbm:.2f} %", f"{15 - mape_lunch_lgbm:.2f} %")
             with col13:
-                st.metric("Random Forest", f"{lunch_pcnt_err_rf:.2f} %", f"{15 - lunch_pcnt_err_rf:.2f} %")
+                st.metric("Random Forest", f"{mape_lunch_rf:.2f} %", f"{15 - mape_lunch_rf:.2f} %")
             
         with col2:
-            st.markdown("### Average error over dinner periods:")
+            st.markdown("### MAPE (dinner):")
+            st.markdown("(Mean Average Percentage Error)")
             
             col21, col22, col23 = st.columns(3)
             with col21:
-                st.metric("Prophet", f"{dinner_pcnt_err_prophet:.2f} %", f"{15 - dinner_pcnt_err_prophet:.2f} %")
+                st.metric("Prophet", f"{mape_dinner_prophet:.2f} %", f"{15 - mape_dinner_prophet:.2f} %")
             with col22:
-                st.metric("LightGBM", f"{dinner_pcnt_err_lgbm:.2f} %", f"{15 - dinner_pcnt_err_lgbm:.2f} %")
+                st.metric("LightGBM", f"{mape_dinner_lgbm:.2f} %", f"{15 - mape_dinner_lgbm:.2f} %")
             with col23:
-                st.metric("Random Forest", f"{dinner_pcnt_err_rf:.2f} %", f"{15 - dinner_pcnt_err_rf:.2f} %")
+                st.metric("Random Forest", f"{mape_dinner_rf:.2f} %", f"{15 - mape_dinner_rf:.2f} %")
 
-            #st.metric("Prophet: Average error (%) over dinner periods:", f"{dinner_pcnt_err:.2f}", f"{15 - dinner_pcnt_err:.2f}")
 
     col1, col2 = st.columns(2)
     #with col1:
     #figure, raw_data = st.tabs(["figure", "raw_data"])
     
     
-    #with figure:
-        #st.dataframe(accu_preprocessed_filtered)
-        #st.dataframe(accu_preprocessed)
     fig0 = create_series_plot_new(accu_preprocessed_filtered)
     st.plotly_chart(fig0, use_container_width=True, theme="streamlit")
     
@@ -119,19 +126,6 @@ if st.session_state["authentication_status"]:
             if submitted:
                 st.warning("Not implemented yet. TODO: Update default model in Keboola")
 
-    # with figure:
-    #     fig1 = create_series_plot_meals(meals_preprocessed_filtered, actuals_preprocesses_filtered)
-    #     st.plotly_chart(fig1, use_container_width=True)
-    # with raw_data:
-    #     st.dataframe(meals_preprocessed_filtered)
-    # #with col2:
-    # figure, raw_data = st.tabs(["figure", "raw_data"])
-    # with figure:
-    #     fig2 = create_series_plot_basic(accu_preprocessed_filtered, actuals_preprocesses_filtered)
-    #     st.plotly_chart(fig2, use_container_width=True)
-    # with raw_data:
-    #     st.dataframe(accu_preprocessed_filtered.loc[accu_preprocessed_filtered.meal_category!=0])
-            
     
     
     

src/helpers.py
@@ -68,8 +68,49 @@ def parse_credentials():
     
     return config_dict
 
+def MAPE(Y_actual,Y_Predicted):
+    """
+    Calculates mean average percentage error
+
+    Parameters
+    ----------
+    Y_actual : 
+        array of actuals.
+    Y_Predicted : 
+        array of forcasted data
+
+    Returns
+    -------
+    mape : float
+        returns the value of the metric.
+
+    """
+    mape = np.mean(np.abs((Y_actual - Y_Predicted)/Y_actual))*100
+    return mape    
+
 @st.cache_data
 def read_df(table_id, index_col=None, date_col=None):
+    """
+    The function reads the table contents from keboola storage using
+    Keboola SAPI client, see 
+    https://github.com/keboola/sapi-python-client
+    
+
+    Parameters
+    ----------
+    table_id : table id of keboola table 
+        DESCRIPTION.
+    index_col : srting, optional
+        possible index column. The default is None.
+    date_col : list, optional
+        list of date columns. The default is None.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
     keboola_client.tables.export_to_file(table_id, '.')
     table_name = table_id.split(".")[-1]
     return pd.read_csv(table_name, index_col=index_col, parse_dates=date_col)
@@ -85,47 +126,39 @@ def calculate_categories(original_dataframe):
 @st.cache_data
 def group_accuracy_df(dataframe):
     dfgrouped = dataframe.groupby(["date", "category", "meal_category"])[["metric_actual", "metric_forecast", "metric_forecast_lgbm", "metric_forecast_rf"]].sum().reset_index()
-    dfgrouped["AE_prophet"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast"])
-    dfgrouped["AE_lgbm"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast_lgbm"])
-    dfgrouped["AE_rf"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast_rf"])
-    dfgrouped["PE_prophet"] = 100 * dfgrouped["AE_prophet"] / dfgrouped["metric_actual"]
-    dfgrouped["PE_lgbm"] = 100 * dfgrouped["AE_lgbm"] / dfgrouped["metric_actual"]
-    dfgrouped["PE_rf"] = 100 * dfgrouped["AE_rf"] / dfgrouped["metric_actual"]
+    dfgrouped["APE_prophet"]= np.abs((dfgrouped["metric_actual"] - dfgrouped["metric_forecast"])/dfgrouped["metric_actual"])
+    dfgrouped["APE_lgbm"]= np.abs((dfgrouped["metric_actual"] - dfgrouped["metric_forecast_lgbm"])/dfgrouped["metric_actual"])
+    dfgrouped["APE_rf"]= np.abs((dfgrouped["metric_actual"] - dfgrouped["metric_forecast_rf"])/dfgrouped["metric_actual"])
     return dfgrouped
 
 def create_summary_table(dataframe, start_date, end_date):
-    dataframe = group_accuracy_df(dataframe)
-    dfgrouped = dataframe.groupby(["date", "category", "meal_category"])[["metric_actual", "metric_forecast", "metric_forecast_lgbm", "metric_forecast_rf"]].sum().reset_index()
-    dfgrouped["AE_prophet"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast"])
-    dfgrouped["AE_lgbm"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast_lgbm"])
-    dfgrouped["AE_rf"]= np.abs(dfgrouped["metric_actual"] - dfgrouped["metric_forecast_rf"])
-    dfgrouped["PE_prophet"] = 100 * dfgrouped["AE_prophet"] / dfgrouped["metric_actual"]
-    dfgrouped["PE_lgbm"] = 100 * dfgrouped["AE_lgbm"] / dfgrouped["metric_actual"]
-    dfgrouped["PE_rf"] = 100 * dfgrouped["AE_rf"] / dfgrouped["metric_actual"]
-    df_filtered = dfgrouped.loc[(dfgrouped.date<=start_date) & (dfgrouped.date<end_date)]
+    dfgrouped = group_accuracy_df(dataframe)
+    df_filtered = dfgrouped.loc[(dfgrouped.date>=start_date) & (dfgrouped.date<=end_date)]
 
     cols = ["metric_actual", 
-            "metric_forecast", 
-            "metric_forecast_lgbm", 
-            "metric_forecast_rf",
-            "PE_prophet",
-            "PE_lgbm",
-            "PE_rf"
-           ]
+        "APE_prophet", 
+        "APE_lgbm", 
+        "APE_rf"
+       ]
 
     dff_grouped = df_filtered.groupby(["category", "meal_category"])[cols]
     summary_df = dff_grouped.agg(
-        mean_pcnt_e_prophet=pd.NamedAgg(column="PE_prophet", aggfunc="mean"),
-        mean_pcnt_e_lgbm=pd.NamedAgg(column="PE_lgbm", aggfunc="mean"),
-        mean_pcnt_e_rf= pd.NamedAgg(column="PE_rf", aggfunc="mean"),       
+        mape_prophet = pd.NamedAgg(column="APE_prophet", aggfunc="mean"),
+        mape_lgbm = pd.NamedAgg(column="APE_lgbm", aggfunc="mean"),
+        mape_rf = pd.NamedAgg(column="APE_rf", aggfunc="mean"),
         ).reset_index()
+    
+    summary_df["mape_prophet"] = 100 * summary_df["mape_prophet"]
+    summary_df["mape_lgbm"] = 100 * summary_df["mape_lgbm"]
+    summary_df["mape_rf"] = 100 * summary_df["mape_rf"]
+    
     summary_df.pivot(columns=["meal_category"], index=["category"])
     summary_pivot_df = summary_df.pivot_table(columns=["meal_category"], index=["category"])
     colnames = ['_'.join([c1, c2]) for c1, c2 in summary_df.pivot_table(columns=["meal_category"], index=["category"]).columns]
     summary_pivot_df.columns=colnames
-    summary_pivot_df["prophet_mean"] = (summary_pivot_df["mean_pcnt_e_prophet_lunch"] + summary_pivot_df["mean_pcnt_e_prophet_dinner"]) / 2
-    summary_pivot_df["lgbm_mean"] = (summary_pivot_df["mean_pcnt_e_lgbm_lunch"] + summary_pivot_df["mean_pcnt_e_lgbm_dinner"]) / 2
-    summary_pivot_df["rf_mean"] = (summary_pivot_df["mean_pcnt_e_rf_lunch"] + summary_pivot_df["mean_pcnt_e_rf_dinner"]) / 2
+    summary_pivot_df["prophet_mean"] = (summary_pivot_df["mape_prophet_lunch"] + summary_pivot_df["mape_prophet_dinner"]) / 2
+    summary_pivot_df["lgbm_mean"] = (summary_pivot_df["mape_lgbm_lunch"] + summary_pivot_df["mape_lgbm_dinner"]) / 2
+    summary_pivot_df["rf_mean"] = (summary_pivot_df["mape_rf_lunch"] + summary_pivot_df["mape_rf_dinner"]) / 2
     
     summary_pivot_df.sort_values(by="prophet_mean", inplace=True)
 